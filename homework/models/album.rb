a8cf66fa78c55bd5e8297a031a35409e7057403d
Fiona Wilson <fionaw237@gmail.com>
2018-08-01 15:44:11 +0100

    Adds save and delete_all methods

---
+++ homework/models/album.rb
@@ -0,0 +1,31 @@
+require("pg")
+require_relative("../db/sql_runner")
+
+class Album
+
+  attr_reader :id, :artist_id
+  attr_accessor :title, :genre
+
+  def initialize(options)
+    @id = options['id'] if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO albums (title, genre, artist_id)
+          VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @artist_id]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i()
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM albums"
+    SqlRunner.run(sql)
+  end
+
+
+
+end
