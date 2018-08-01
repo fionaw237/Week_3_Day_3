9b4562952878730a24b5209ab924e91a4c433732
Fiona Wilson <fionaw237@gmail.com>
2018-08-01 18:18:09 +0100

    Adds delete method

--- homework/models/artist.rb
+++ homework/models/artist.rb
@@ -42,6 +42,12 @@ class Artist
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM artists WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.find(id)
     sql = "SELECT * FROM artists WHERE id = $1"
     values = [id]
